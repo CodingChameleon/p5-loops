9227903151b1e65b1724d0e6efe9a981c091175d
Naia McNatt <nmcnatt@wlapcs.org>
2017-04-08 21:13:40 +0000

    first try

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup(){
+    createCanvas(600,400);
+    background(0);
+}
+
+function draw(){
+    strokeWeight(4);
+    stroke(255);
+   
+    var x = 0;
+    while(x < width){
+        fill(225, 35, 1);
+        ellipse(x, 255, 25, 25);
+        x = x + 50;
+    }
+    
+    for(var x = 0; x < 650; x = x + 10){
+        fill(70, 157, 219);
+        ellipse(x, 255, 25, 25);
+    }
+}
